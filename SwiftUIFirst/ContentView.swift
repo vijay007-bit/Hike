10a3a020cafe54ae0045e12c760798c0bbb72b0d
Vijay Vikram Singh <svijayvikram00@gmail.com>
2024-02-07 19:09:24 +0530

    Initial Commit

---
+++ SwiftUIFirst/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIFirst
+//
+//  Created by Vijay Singh on 07/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIFirst/SwiftUIFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirstApp.swift
+//  SwiftUIFirst
+//
+//  Created by Vijay Singh on 07/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
